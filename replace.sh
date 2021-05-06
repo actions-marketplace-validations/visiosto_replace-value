2ed80f9da991a1b89e596c2903b5c7209b3fa324
Antti Kivi <antti.kivi@visiosto.fi>
2021-05-06 15:34:38 +0300

    feat: print the inputs

--- replace.sh
+++ replace.sh
@@ -5,6 +5,11 @@
 
 set -e
 
+echo "The inputs are:"
+echo "file: $INPUT_FILE"
+echo "replaced: $INPUT_REPLACED"
+echo "value: $INPUT_VALUE"
+
 sed -i -- "s/${INPUT_REPLACED}/${INPUT_VALUE}/g" "${GITHUB_WORKSPACE}/${INPUT_FILE}"
 echo "The file is now:\n"
 cat $INPUT_FILE
